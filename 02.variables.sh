925972e28360a23309d2c6c4ffc63ce0a73cff7a
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-05-16 15:17:08 +0530

    expense shell edited

--- 02.variables.sh
+++ 02.variables.sh
@@ -5,3 +5,10 @@ person2=$2
 
 echo "$person1: Hi $person2, how are you?"
 echo "$person2: I'm good $person1, how about you?"
+
+echo "please enter your username"
+read USERNAME
+echo "username is $USERNAME"
+
+echo "enter you passcode"
+read -s PASSCODE
