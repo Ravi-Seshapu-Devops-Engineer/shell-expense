08820649ed3dba1d18b898623fdd0fda5852770b
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-05-17 16:00:56 +0530

    expense shell edited

--- 03.mixup.sh
+++ 03.mixup.sh
@@ -10,15 +10,15 @@ LOGS_FOLDER="/var/log/shell-expense"
 LOGS_FILE="/var/log/shell-expense/$0.log"
 
 if [ $USERID -ne 0 ]; then
-  echo -e "$R Please run the script with root user access $N"
+  echo -e "$R Please run the script with root user access $N" | tee $LOGS_FILE
   exit 1
 fi
 
 validate(){
   if [ $1 -ne 0 ]; then
-    echo -e "$G installing $2 success"
+    echo -e "$G installing $2 success" | tee $LOGS_FILE
   else
-    echo -e "$R installing $2 failed"
+    echo -e "$R installing $2 failed" | tee $LOGS_FILE
   fi
 }
 
@@ -26,8 +26,8 @@ for package in $@
 do
   dnf list installed $package
   if [ $? -ne 0 ]; then
-    echo -e "$Y The given package is no installed. Installing now $N"
-    dnf install $package -Y
+    echo -e "$Y The given package is not installed. Installing now $N"
+    dnf install $package -y
     validate $? $package
   else
     echo -e "$Y already exists skipping..$N"
