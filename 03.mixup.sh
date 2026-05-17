74d6386788083fac0d71c33bf97cb0a29e27ef1f
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-05-17 15:46:33 +0530

    expense shell edited

--- 03.mixup.sh
+++ 03.mixup.sh
@@ -10,7 +10,7 @@ LOGS_FOLDER="/var/log/shell-expense"
 LOGS_FILE="/var/log/shell-expense/$0.log"
 
 if [ $USERID -ne 0 ]; then
-  echo "$R Please run the script with root user access $N"
+  echo -e "$R Please run the script with root user access $N"
   exit 1
 fi
 
