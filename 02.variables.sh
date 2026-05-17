ff9980091354b3acd9d2fde1203eed329af01737
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-05-17 14:23:11 +0530

    expense shell edited

--- 02.variables.sh
+++ 02.variables.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
-person1=$1
-person2=$2
+# person1=$1
+# person2=$2
 
-echo "$person1: Hi $person2, how are you?"
-echo "$person2: I'm good $person1, how about you?"
+# echo "$person1: Hi $person2, how are you?"
+# echo "$person2: I'm good $person1, how about you?"
 
-echo "please enter your username"
-read USERNAME
-echo "username is $USERNAME"
+# echo "please enter your username"
+# read USERNAME
+# echo "username is $USERNAME"
 
-echo "enter you passcode"
-read -s PASSCODE
+# echo "enter you passcode"
+# read -s PASSCODE
 
 
-START_TIME=$(date +%s)
-#echo "script stated executing at $START_TIME"
-sleep 10
-END_Time=$(date +%s)
-total_time=$(($END_Time-$START_TIME))
-echo "script executed in $total_time seconds"
+# START_TIME=$(date +%s)
+# #echo "script stated executing at $START_TIME"
+# sleep 10
+# END_Time=$(date +%s)
+# total_time=$(($END_Time-$START_TIME))
+# echo "script executed in $total_time seconds"
 
-##special variables
-echo "all variables passed to the script: $@"
-echo "no of variables passed to the script: $#"
-echo "status code of the previous command: $?"
-echo "present working directory: $PWD"
-echo "script name: $0"
-echo "who is running the script: $USER"
-echo "PID od the script: $$"
-sleep 100 &
-echo "bckground process id: $!"
+# ##special variables
+# echo "all variables passed to the script: $@"
+# echo "no of variables passed to the script: $#"
+# echo "status code of the previous command: $?"
+# echo "present working directory: $PWD"
+# echo "script name: $0"
+# echo "who is running the script: $USER"
+# echo "PID od the script: $$"
+# sleep 100 &
+# echo "bckground process id: $!"
 
+#datatypes array
+
+fruits=("Apple" "banana" "mango")
+first_fruit=${fruits(0)}
+echo "$first_fruit"
